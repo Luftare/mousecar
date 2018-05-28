19eac7a92c77cfd7c17772b0a275cb3e5ae16765
Ilmari Koskinen <ilmari.koskinen@houston-inc.com>
2018-05-28 12:28:06 +0300

    remove watch

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,18 +1,18 @@
-const path = require("path");
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = options => {
+module.exports = (options) => {
   return {
     entry: './src/index.js',
-    watch: true,
+    watch: false,
     output: {
       path: path.resolve(__dirname, 'build'),
-      filename: 'bundle.js',
+      filename: 'bundle.js'
     },
     plugins: [
       new HtmlWebpackPlugin({
         template: 'index.html'
       })
     ]
-  }
-}
+  };
+};
